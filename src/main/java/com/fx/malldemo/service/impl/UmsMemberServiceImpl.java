5886af7c9ee0b94a3a02740b40ba15d2616f9fc6
mabaofeng <oneinchofsunshine@163.com>
2019-08-29 17:29:42 +0800

    redis

---
+++ src/main/java/com/fx/malldemo/service/impl/UmsMemberServiceImpl.java
@@ -0,0 +1,21 @@
+package com.fx.malldemo.service.impl;
+
+import com.fx.malldemo.common.CommonResult;
+import com.fx.malldemo.service.UmsMemberService;
+
+/**
+ * @auther: mabaofeng
+ * @date: 2019/8/29 10:10
+ * @description:
+ */
+public class UmsMemberServiceImpl implements UmsMemberService {
+    @Override
+    public CommonResult generateAuthCode(String telephone) {
+        return null;
+    }
+
+    @Override
+    public CommonResult verifyAuthCode(String telephone, String authCode) {
+        return null;
+    }
+}
